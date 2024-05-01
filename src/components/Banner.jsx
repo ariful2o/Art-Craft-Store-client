dd44a277f03f5bb703249c7860e36664fb2e28f4
mohammadariful20 <arifulz848@gmail.com>
2024-05-01 21:22:49 +0600

    update filter function

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,25 @@
+import { Typewriter } from "react-simple-typewriter";
 
 export default function Banner() {
     return (
         <div className="hero min-h-96" style={{ backgroundImage: 'url(https://capricathemes.com/prestashop/PRS07/PRS070168/img/cms/cms-banner.jpg)' }}>
             <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content  text-neutral-content ">
+            <div className="hero-content text-center text-neutral-content ">
                 <div className="max-w-md">
+
                     <h1 className="mb-3 text-5xl font-bold">30% Save</h1>
-                    <h1 className="mb-5 text-5xl">Latest Ceramic Art</h1>
+                    <h1 className="mb-5 text-5xl block">Latest
+                        <Typewriter
+                            words={[' Ceramic Art', ' Wooden Crafts', ' Japanese ceramic']}
+                            loop={85}
+                            cursor
+                            cursorStyle='|'
+                            typeSpeed={90}
+                            deleteSpeed={50}
+                            delaySpeed={1000}
+
+                        />
+                    </h1>
                     <p className="mb-5">There are many variations of passages of them.</p>
                     <button className="btn">Get Started</button>
                 </div>
--- src/pages/AllArtCraft/MyListArtCraft.jsx
+++ src/pages/AllArtCraft/MyListArtCraft.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import Swal from "sweetalert2";
+import Select from 'react-select';
+
+const options = [
+  { value: 'yes', label: 'Yes' },
+  { value: 'no', label: 'No' },
+];
 
 export default function MyListArtCraft() {
+  const [selectedOption, setSelectedOption] = useState(null); 
+
   const myArtCraftLoaded = useLoaderData()
   const [myArtCraft, setMyArtCraft] = useState(myArtCraftLoaded)
   const handleDelete = (id) => {
@@ -30,13 +38,29 @@ export default function MyListArtCraft() {
       }
     });
   }
+  // Function to handle change in the selected option
+  const handleOptionChange = (selectedOption) => {
+    setSelectedOption(selectedOption); // Update the state with the selected option
+    const filter=myArtCraftLoaded.filter(item=>item.customization.toLowerCase()===selectedOption.value.toLowerCase())
+    setMyArtCraft(filter)
+  };
 
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3  my-10 gap-8">
-     
-      {
-        !myArtCraft ? <div className="justify-center flex my-20"><span className="loading loading-spinner loading-lg"></span></div> : myArtCraft.map(item => {
+    <>
+      <h2 className="text-4xl text-center font-bold my-10 text-[#70D2C0]">My Art & Craft : {myArtCraft.length}</h2>
+      <div className="flex items-center font-bold p-9">
+      Filter By Customization:
+      <Select
+        className="min-w-60 pl-2 font-normal"
+        value={selectedOption} // Set the value prop to selectedOption
+        onChange={handleOptionChange} // Call handleOptionChange when the option changes
+        options={options}
+      />
+    </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3  my-10 gap-8">
+        {!myArtCraft ? <div className="justify-center flex my-20"><span className="loading loading-spinner loading-lg"></span></div> : myArtCraft.map(item => {
           return (
             <div key={item._id} className="card w-full md:w-96 bg-base-100 shadow-xl mx-auto">
               <figure><img className="w-full h-72 p-3" src={item.image} alt={item.item_name} /></figure>
@@ -56,9 +80,8 @@ export default function MyListArtCraft() {
                 </div>
               </div>
             </div>
-          )
-        })
-      }
-    </div>
+          );
+        })}
+      </div></>
   )
 }
